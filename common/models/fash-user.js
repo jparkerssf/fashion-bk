4e2f3e4c85b5011d8eec81c07d706b298b27748b
jparkerssf <jparker@softstackfactory.org>
2016-07-23 15:48:57 +0000

    day 10

--- common/models/fash-user.js
+++ common/models/fash-user.js
@@ -1,3 +1,17 @@
 module.exports = function(Fashuser) {
+    
+      Fashuser.observe('after save', function(ctx, next) {
+        if(ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) {
+                if(err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }else {
+             next();
+        }
+    })
+  
+  
 
 };
